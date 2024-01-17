1ba5a69a4a40ed9e3a5f614201ff215082215d99
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-01-18 00:00:00 +0100

    Solved group anagrams

---
+++ NeetCode/CSharp/ArraysAndHashing/GroupAnagrams/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Solution
+{
+
+    public static void Main()
+    {
+        Solution solution = new Solution();
+
+        PrintResult(solution.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" })); //Expected true
+        PrintResult(solution.GroupAnagrams(new string[] { "" })); //Expected false
+        PrintResult(solution.GroupAnagrams(new string[] { "a" })); //Expected true
+    }
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, List<string>> anagramGroups = new Dictionary<string, List<string>>();
+        foreach (string str in strs)
+        {
+            int[] count = new int[26];
+
+            foreach (char c in str)
+            {
+                count[c - 'a']++;
+            }
+
+            string key = string.Join(",", count);
+
+            if (!anagramGroups.ContainsKey(key))
+            {
+                anagramGroups.Add(key,new List<string>());
+            }
+
+            anagramGroups[key].Add(str);
+        }
+
+        return anagramGroups.Values.ToList<IList<string>>();
+    }
+
+        private static void PrintResult(IList<IList<string>> result)
+    {
+        foreach (var group in result)
+        {
+            Console.WriteLine($"[{string.Join(", ", group)}]");
+        }
+
+        Console.WriteLine();
+    }
+}
