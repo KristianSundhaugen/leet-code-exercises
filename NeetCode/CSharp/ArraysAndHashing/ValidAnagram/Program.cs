e6c3da8ba0d746a64a6ce604126ca11d7bd09f29
kristiansundhaugen <kristiansundhaugen@gmail.com>
2024-01-19 18:34:18 +0100

    Did Valid Anagram leetcode

---
+++ NeetCode/CSharp/ArraysAndHashing/ValidAnagram/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Solution
+{
+    public static void Main()
+    {
+        Solution solution = new Solution();
+
+        Console.WriteLine(solution.IsAnagram("anagram", "nagaram")); //Expected true
+        Console.WriteLine(solution.IsAnagram("rat", "car")); //Expected false
+    }
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        char[] sChars = s.ToLower().ToCharArray();
+        char[] tChars = t.ToLower().ToCharArray();
+
+        int[] countLetterArrayS = new int[26];
+        int[] countLetterArrayT = new int[26];
+
+        for (int i = 0; i < sChars.Length; i++)
+        {
+            countLetterArrayS[sChars[i] - 'a']++;
+            countLetterArrayT[tChars[i] - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (countLetterArrayS[i] != countLetterArrayT[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
