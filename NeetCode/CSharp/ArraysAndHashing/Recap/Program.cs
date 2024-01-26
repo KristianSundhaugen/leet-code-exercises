04276520c4b0f4d6acc1fb157c229bc86ec628f4
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-01-26 13:03:25 +0100

    Solved TwoSum challange again

--- NeetCode/CSharp/ArraysAndHashing/Recap/Program.cs
+++ NeetCode/CSharp/ArraysAndHashing/Recap/Program.cs
@@ -4,25 +4,36 @@
     {
         public static void Main()
         {
-            int[] result1 = Solution(new int[] { 1,2,3,1 });
-            Console.WriteLine("[{0}]", string.Join(", ", result1)); //Expected [1,2,1,1,2,1]
-            int[] result2 = Solution(new int[] { 1, 3, 2, 1 });
-            Console.WriteLine("[{0}]", string.Join(", ", result2)); //Expected [1,3,2,1,1,3,2,1]
+            int[] result1 = Solution(new int[] { 2,7,11,15 }, 9);
+            Console.WriteLine("[{0}]", string.Join(", ", result1)); //Expected [0,1]
+            int[] result2 = Solution(new int[] { 3,2,4 }, 6);
+            Console.WriteLine("[{0}]", string.Join(", ", result2)); //Expected [1,2]
+            int[] result3 = Solution(new int[] { 3,3 }, 6);
+            Console.WriteLine("[{0}]", string.Join(", ", result3)); //Expected [0,1]
         }
 
-        public static int[] Solution(int[] nums)
+        public static int[] Solution(int[] nums, int target)
         {
-            int numsLength = nums.Length;
+            Dictionary<int,int> dictionary = new();
 
-            int[] answer = new int[nums.Length * 2];
-
-            for (int i = 0; i < numsLength; i++)
+            for (int i = 0; i < nums.Length; i++)
             {
-                answer[i] = nums[i];
-                answer[i + numsLength] = nums[i];
+                if(nums[i]>target-1){
+                    continue;
+                }
+
+                int neededNumber = target-nums[i];
+
+                if(dictionary.ContainsKey(neededNumber)){
+                    return new int[] {dictionary[neededNumber], i};
+                }
+                
+                if(!dictionary.ContainsKey(nums[i])){
+                    dictionary.Add(nums[i], i);
+                }
             }
 
-            return answer;
+            return new int[] {};
         }
     }
 }
