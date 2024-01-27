a685d46069a2ac8f91c1b0b4a73e4be3a61f1836
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-01-27 17:57:01 +0100

    Solved IsSubsequence

---
+++ NeetCode/CSharp/ArraysAndHashing/IsSubsequence/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Recap
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Solution("abc", "ahbgdc")); //Expected true
+            Console.WriteLine(Solution("axc", "ahbgdc")); //Expected false
+        }
+
+        public static bool Solution(string s, string t)
+        {
+            int i = 0;
+            int j = 0;
+
+            while((i < s.Length) && (j < t.Length)){
+                if(s[i] == t[j]){
+                    i++;
+                }
+                j++;
+            }
+
+            if(i == s.Length){
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
