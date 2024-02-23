806d38ff9f5c39030df3b3ecb47a458629824f87
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-02-23 14:23:34 +0100

    Solved Udemy ReverseString

---
+++ UdemyCourse/Exercises/ReverseString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static string ReverseStrig(string str)
+    {
+        char[] chars = str.ToCharArray();
+        int j = 0;
+
+        //Can use Array.Reverse(chars)
+        for (int i = str.Length-1; i >= 0; i--)
+        {
+            chars[j] = str[i];
+            j++;
+        } 
+
+        return new string(chars);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ReverseStrig("Hello world, how are you doing?"));
+    }
+}
