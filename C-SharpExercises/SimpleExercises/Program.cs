4afc377c1e60bc21df090d38d127af837b9b980f
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-04-25 17:53:08 +0200

    Exercise 4

--- C-SharpExercises/SimpleExercises/Program.cs
+++ C-SharpExercises/SimpleExercises/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void SimpleExercise()
     {
-        System.Console.WriteLine("2 / 2 = " + (2/2));
+        System.Console.WriteLine("-1 + 4 * 6 = " + (-1 + 4 * 6));
+        System.Console.WriteLine("(35+5) % 7 = " + ((35+5) % 7));
+        System.Console.WriteLine("14 + -4 * 6 / 11 = " + (14 + -4 * 6 / 11));
+        System.Console.WriteLine("2 + 15 / 7 * 1 - 7 % 2 = " + (2 + 15 / 7 * 1 - 7 % 2));
     }
 
     static void Main(string[] args)
