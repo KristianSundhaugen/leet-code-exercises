8984b61e656b76d3b9ce2add71f6b25781c6150e
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-04-25 22:12:26 +0200

    Exercise 7

--- C-SharpExercises/SimpleExercises/Program.cs
+++ C-SharpExercises/SimpleExercises/Program.cs
@@ -4,18 +4,18 @@ class Program
 {
     static void SimpleExercise()
     {
-        int num1, num2, num3;
+        int num1, num2;
 
         System.Console.WriteLine("Write first number:");
         num1 = Convert.ToInt32(System.Console.ReadLine());
         System.Console.WriteLine("Write first second:");
         num2 = Convert.ToInt32(System.Console.ReadLine());
-        System.Console.WriteLine("Write first third:");
-        num3 = Convert.ToInt32(System.Console.ReadLine());
 
-        int result = num1 * num2 * num3;
-
-        System.Console.WriteLine("Result: {0} * {1} * {2} = {3}", num1, num2, num3, result);
+        System.Console.WriteLine("Result: {0} + {1} = {2}", num1, num2, (num1+num2));
+        System.Console.WriteLine("Result: {0} - {1} = {2}", num1, num2, (num1-num2));
+        System.Console.WriteLine("Result: {0} * {1} = {2}", num1, num2, (num1*num2));
+        System.Console.WriteLine("Result: {0} / {1} = {2}", num1, num2, (num1/num2));
+        System.Console.WriteLine("Result: {0} mod {1} = {2}", num1, num2, (num1%num2));
     }
 
     static void Main(string[] args)
