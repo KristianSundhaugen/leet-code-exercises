b498946468377d1b0e4baf28f7ceb5ee1471239b
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-04-26 10:57:52 +0200

    Exercise 15 completed

--- C-SharpExercises/SimpleExercises/Program.cs
+++ C-SharpExercises/SimpleExercises/Program.cs
@@ -4,17 +4,26 @@ class Program
 {
     static void SimpleExercise()
     {
-        Console.WriteLine("Write a number");
-        int digit = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Write a string");
+        string inputString = Console.ReadLine();
+        int stringLength = inputString.Length;
 
-        for (int i = 1; i <= digit; i++){
-            if(i==1 || i == digit){
-                Console.WriteLine("{0}{0}{0}", digit);
-            } else {
-                Console.WriteLine("{0} {0}", digit);
-            }
+        if (stringLength <= 1)
+        {
+            Console.WriteLine("First and last letters reversed");
+            Console.WriteLine(inputString);
+            return;
         }
+
+        char startCharacter = inputString[0];
+        char endCharacter = inputString[stringLength - 1];
+
+        inputString = endCharacter + inputString.Substring(1, stringLength - 2) + startCharacter;
+
+        Console.WriteLine("First and last letters reversed");
+        Console.WriteLine(inputString);
     }
+
 
     static void Main(string[] args)
     {
